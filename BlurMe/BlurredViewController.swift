efea2ed13b1b680848a93edb8f20aa90903acba4
Daniel Norton <daniel.norton@brimstead.com>
2016-01-06 18:49:54 -0600

    experiment

---
+++ BlurMe/BlurredViewController.swift
@@ -0,0 +1,18 @@
+//
+//  BlurredViewController.swift
+//  BlurMe
+//
+//  Created by Daniel Norton on 1/3/16.
+//  Copyright © 2016 brimstead. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class BlurredViewController : UIViewController {
+    
+    @IBAction func didTapDone(sender: UIButton) {
+        
+        presentingViewController?.dismissViewControllerAnimated(true, completion: nil)
+    }
+}
