4465497d0041eb7d4f5346765a21e971988411b0
Daniel Norton <daniel.norton@brimstead.com>
2016-01-06 22:29:40 -0600

    reproduced storyboard in code

--- BlurMe/ViewController.swift
+++ BlurMe/ViewController.swift
@@ -10,16 +10,159 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    // MARK: - UIViewController
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        for case let button as UIButton in view.subviews {
+            
+            if let label = button.titleLabel {
+                
+                label.adjustsFontSizeToFitWidth = true
+                label.minimumScaleFactor = 0.5
+            }
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    // MARK: - ViewController
+    // MARK: IBActions
+    @IBAction func didTapCode(sender: UIButton) {
+        
+        presentViaCode()
     }
+    
+    
+    // MARK: public methods
+    func didTapDone(sender: UIGestureRecognizer) {
+        
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
+    // MARK: private methods
+    private func presentViaCode() {
+        
+        let controller = UIViewController()
+        controller.modalPresentationStyle = .OverCurrentContext
+        let superview = controller.view
+        
+        let subview = UIVisualEffectView(effect: UIBlurEffect(style: .Light))
+        subview.translatesAutoresizingMaskIntoConstraints = false
+        superview.addSubview(subview)
+        
+        superview.addConstraint(NSLayoutConstraint(item: subview
+            , attribute: NSLayoutAttribute.Baseline
+            , relatedBy: .Equal
+            , toItem: superview
+            , attribute: .Baseline
+            , multiplier: 1.0
+            , constant: 0.0))
+        
+        superview.addConstraint(NSLayoutConstraint(item: subview
+            , attribute: NSLayoutAttribute.Leading
+            , relatedBy: .Equal
+            , toItem: superview
+            , attribute: .Leading
+            , multiplier: 1.0
+            , constant: 0.0))
 
+        superview.addConstraint(NSLayoutConstraint(item: subview
+            , attribute: NSLayoutAttribute.Trailing
+            , relatedBy: .Equal
+            , toItem: superview
+            , attribute: .Trailing
+            , multiplier: 1.0
+            , constant: 0.0))
 
+        superview.addConstraint(NSLayoutConstraint(item: subview
+            , attribute: NSLayoutAttribute.Top
+            , relatedBy: .Equal
+            , toItem: superview
+            , attribute: .CenterY
+            , multiplier: 1.0
+            , constant: -55.0))
+        
+        
+        let labelView = UIVisualEffectView(effect: UIVibrancyEffect(forBlurEffect: UIBlurEffect(style: .Light)))
+        labelView.translatesAutoresizingMaskIntoConstraints = false
+        subview.contentView.addSubview(labelView)
+        
+        subview.addConstraint(NSLayoutConstraint(item: labelView
+            , attribute: NSLayoutAttribute.Baseline
+            , relatedBy: .Equal
+            , toItem: subview
+            , attribute: .Baseline
+            , multiplier: 1.0
+            , constant: 0.0))
+        
+        subview.addConstraint(NSLayoutConstraint(item: labelView
+            , attribute: NSLayoutAttribute.Leading
+            , relatedBy: .Equal
+            , toItem: subview
+            , attribute: .Leading
+            , multiplier: 1.0
+            , constant: 0.0))
+        
+        subview.addConstraint(NSLayoutConstraint(item: labelView
+            , attribute: NSLayoutAttribute.Trailing
+            , relatedBy: .Equal
+            , toItem: subview
+            , attribute: .Trailing
+            , multiplier: 1.0
+            , constant: 0.0))
+        
+        labelView.addConstraint(NSLayoutConstraint(item: labelView
+            , attribute: NSLayoutAttribute.Height
+            , relatedBy: .Equal
+            , toItem: nil
+            , attribute: .Height
+            , multiplier: 1.0
+            , constant: 100.0))
+        
+        
+        let label = UILabel()
+        label.text = "Done"
+        label.font = UIFont.systemFontOfSize(70.0)
+        label.textAlignment = .Center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        labelView.contentView.addSubview(label)
+        
+        labelView.addConstraint(NSLayoutConstraint(item: label
+            , attribute: NSLayoutAttribute.Baseline
+            , relatedBy: .Equal
+            , toItem: labelView
+            , attribute: .Baseline
+            , multiplier: 1.0
+            , constant: 0.0))
+        
+        labelView.addConstraint(NSLayoutConstraint(item: label
+            , attribute: NSLayoutAttribute.Leading
+            , relatedBy: .Equal
+            , toItem: labelView
+            , attribute: .Leading
+            , multiplier: 1.0
+            , constant: 0.0))
+        
+        labelView.addConstraint(NSLayoutConstraint(item: label
+            , attribute: NSLayoutAttribute.Trailing
+            , relatedBy: .Equal
+            , toItem: labelView
+            , attribute: .Trailing
+            , multiplier: 1.0
+            , constant: 0.0))
+        
+        labelView.addConstraint(NSLayoutConstraint(item: label
+            , attribute: NSLayoutAttribute.Top
+            , relatedBy: .Equal
+            , toItem: labelView
+            , attribute: .Top
+            , multiplier: 1.0
+            , constant: 0.0))
+        
+        labelView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: "didTapDone:"))
+        self.presentViewController(controller, animated: true, completion: nil)
+    }
 }
 
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 #BlurMe#
-A very simply test of [UIVisualEffectView](https://developer.apple.com/library/ios/documentation/UIKit/Reference/UIVisualEffectView/).
+A very simply test of [UIVisualEffectView](https://developer.apple.com/library/ios/documentation/UIKit/Reference/UIVisualEffectView/) done in the storyboard and in code.
 
 ![](show.png)
